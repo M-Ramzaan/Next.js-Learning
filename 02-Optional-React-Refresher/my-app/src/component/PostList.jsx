88eeafa7a407aa7ebf5aaed201351cd2735c14ac
Muhammad Ramzan Akram <ramzanakram1875@outlook.com>
2024-11-19 05:58:40 +0500

    Learned till Lifting State Up

--- 02-Optional-React-Refresher/my-app/src/component/PostList.jsx
+++ 02-Optional-React-Refresher/my-app/src/component/PostList.jsx
@@ -1,15 +1,34 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
+import { useState } from "react";
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
-    <ul className={classes.posts}>
-      <li>
-        <Post author="Ramzan" body="React.js is awesome." />
-      </li>
-      <li>
-        <Post author="Mirha" body="Check out the full course." love="Nice" />
-      </li>
-    </ul>
+    <>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
+      <ul className={classes.posts}>
+        <li>
+          <Post author={enteredAuthor} body={enteredBody} />
+        </li>
+        <li>
+          <Post author="Mirha" body="Please check out the full course!" />
+        </li>
+      </ul>
+    </>
   );
 }
 
