aa49f95683ce918f7f817136c01911f9e5a8de8e
Muhammad Ramzan Akram <ramzanakram1875@outlook.com>
2024-12-07 20:10:21 +0500

    Working on 2 App

---
+++ 02-React-Project-02/my-app-02/src/App.js
@@ -0,0 +1,13 @@
+import Todo from "./Components/Todo";
+function App() {
+  return (
+    <div>
+      <h1>My To Do</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
